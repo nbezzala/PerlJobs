23f08f095badd7746d4bf1dce4b9fbbd007ce2c6
Nitish Bezzala <nbezzala@yahoo.com>
2009-09-26 17:13:43 +0530

    fixed company create with address

--- lib/PerlJobs/Form/Company.pm
+++ lib/PerlJobs/Form/Company.pm
@@ -4,6 +4,8 @@ use HTML::FormHandler::Moose;
 extends 'HTML::FormHandler::Model::DBIC';
 with 'HTML::FormHandler::Render::Table';
 
+has '+item_class' => ( default => 'Company' );
+
 has_field 'name' => ( type => 'Text' );
 
 has_field 'address_id' => ( type => '+PerlJobs::Form::Field::Address', label => 'Address' );
