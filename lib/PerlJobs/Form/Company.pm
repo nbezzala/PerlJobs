262e0b1c4746a09b3e341681a3b91f6219502ada
Nitish Bezzala <nbezzala@yahoo.com>
2009-09-27 12:21:55 +0530

    Added comments and license.

--- lib/PerlJobs/Form/Company.pm
+++ lib/PerlJobs/Form/Company.pm
@@ -1,5 +1,18 @@
 package PerlJobs::Form::Company;
 
+=head1 NAME
+
+PerlJobs::Controller::Companies - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Create and Edit Company Form.
+
+=head1 METHODS
+
+=cut
+
+
 use HTML::FormHandler::Moose;
 extends 'HTML::FormHandler::Model::DBIC';
 with 'HTML::FormHandler::Render::Table';
@@ -13,4 +26,16 @@ has_field 'address_id' => ( type => '+PerlJobs::Form::Field::Address', label => 
 has_field 'submit' => ( type => 'Submit', value => 'Submit' );
 
 no HTML::FormHandler::Moose;
+
+=head1 AUTHOR
+
+Nitish Bezzala (nbezzala@yahoo.com)
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
 1;
